7da5f525322fe9c9d7a3893be0e47458b30eaaff
pierrout <gabrielpierrout@outlook.com.br>
2021-08-13 00:17:17 -0300

    First exercises

---
+++ Estrutura Sequencial/ex01.py
@@ -0,0 +1,5 @@
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+num = int(input("Digite o número desejado: "))
+print("O número informado foi, {}".format(num))
+
+
